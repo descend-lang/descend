67ff9bade7834712612873d3103f52bb53efdb98
l_spar02 <l_spar02@uni-muenster.de>
2021-02-25 23:30:11 +0100

    get_span_position (line/column)

--- src/ast/span.rs
+++ src/ast/span.rs
@@ -10,4 +10,22 @@ impl Span {
     pub fn new(start: usize, end: usize) -> Self {
         Self { start, end }
     }
+    
+    pub fn get_span_position(&self, text: &str) -> (usize, usize){
+        let mut line_count = 1;
+        let mut column_count = 0;
+        let bytes = text.as_bytes();
+        for (i, &letter) in bytes.iter().enumerate(){
+            if letter == b'\n' {
+                line_count = line_count + 1;
+                column_count = 1;
+            } else {
+                column_count = column_count + 1;
+            }
+            if i <= self.start {
+                return (line_count, column_count);
+            }
+        }
+        (line_count, column_count)
+    }
 }
