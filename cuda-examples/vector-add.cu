1cacf5107691cb3a8374278d1fa051e127f4a69d
Köpcke <bastian.koepcke@wwu.de>
2021-03-06 16:10:57 +0100

    make names more concise

--- cuda-examples/vector-add.cu
+++ cuda-examples/vector-add.cu
@@ -3,20 +3,20 @@
 
 template<std::size_t n>
 auto inplace_vector_add_host(
-    descend::i32_t * const __restrict__ ha_array,
-    const descend::i32_t * const __restrict__ hb_array
+    descend::i32 * const __restrict__ ha_array,
+    const descend::i32 * const __restrict__ hb_array
 ) -> void {
-    const auto gpu = descend::create_gpu(0);
+    const auto gpu = descend::gpu(0);
 
-    auto a_array = descend::gpu_alloc<descend::array_t<descend::i32_t, n>>(&gpu, ha_array);
-    const auto b_array = descend::gpu_alloc<descend::array_t<descend::i32_t, n>>(&gpu, hb_array);
+    auto a_array = descend::gpu_alloc<descend::array<descend::i32, n>>(&gpu, ha_array);
+    const auto b_array = descend::gpu_alloc<descend::array<descend::i32, n>>(&gpu, hb_array);
 
     const auto grid = descend::spawn_threads<1, 1024>(&gpu);
-    descend::par_for_across(
+    descend::par_for(
         &grid,
         [] __device__ (
-            descend::i32_t * const __restrict__ a_array,
-            const descend::i32_t * const __restrict__ b_array
+            descend::i32 * const __restrict__ a_array,
+            const descend::i32 * const __restrict__ b_array
         ) {
             int g_tid = blockIdx.x * blockDim.x + threadIdx.x;
             a_array[g_tid] = a_array[g_tid] + b_array[g_tid];
@@ -25,12 +25,12 @@ auto inplace_vector_add_host(
         &b_array
     );
 
-    descend::copy_to_host<descend::array_t<descend::i32_t, n>>(&a_array, ha_array);
+    descend::copy_to_host<descend::array<descend::i32, n>>(&a_array, ha_array);
 }
 
 auto main() -> int {
-    auto ha_array = descend::HeapBuffer<descend::array_t<descend::i32_t, 1*1024>>(descend::create_array<1024, descend::i32_t>(0));
-    const auto hb_array = descend::HeapBuffer<descend::array_t<descend::i32_t, 1*1024>>(descend::create_array<1024, descend::i32_t>(1));
+    auto ha_array = descend::HeapBuffer<descend::array<descend::i32, 1*1024>>(descend::create_array<1024, descend::i32>(0));
+    const auto hb_array = descend::HeapBuffer<descend::array<descend::i32, 1*1024>>(descend::create_array<1024, descend::i32>(1));
     inplace_vector_add_host<1*1024>(&ha_array, &hb_array);
 
     for (size_t i = 0; i < 1*1024; i++) {
