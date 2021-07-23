a21c9614244e096348febab4db6c76474f643acb
Olliewer <steinhauer.oliver@online.de>
2021-07-23 20:34:20 +0200

    codegen/mod.rs mergeconfilct resolved

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1383,7 +1383,7 @@ fn gen_ty(ty: &desc::TyKind, mutbl: desc::Mutability) -> cu::Ty {
     let cu_ty = match ty {
         Ident(ident) => cu::Ty::Ident(ident.name.clone()),
         Fn(_, _, _, _) => unimplemented!("needed?"),
-        Data(d::Scalar(s)) => match s {
+        Data(d::Scalar(s)) |  Data(d::Atomic(s)) => match s {
             desc::ScalarTy::Unit => cu::Ty::Scalar(cu::ScalarTy::Void),
             desc::ScalarTy::I32 => cu::Ty::Scalar(cu::ScalarTy::I32),
             desc::ScalarTy::F32 => cu::Ty::Scalar(cu::ScalarTy::F32),
