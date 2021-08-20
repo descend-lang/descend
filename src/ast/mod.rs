4c1d56e54779ed8f7ab70f867aaa55873ce2e029
Olliewer <steinhauer.oliver@online.de>
2021-08-20 10:20:46 +0200

    added u32 data type, added new example for computed indexing, improved bfs

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1145,6 +1145,7 @@ pub enum Nat {
 
 impl Nat {
     pub fn eval(&self) -> Result<usize, String> {
+        println!("\tuse nat!! {}", self);
         match self {
             Nat::Ident(i) => Err(format!("Cannot evaluate identifier `{}`.", i)),
             Nat::Lit(n) => Ok(*n),
