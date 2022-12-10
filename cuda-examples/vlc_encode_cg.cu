2e5a468f64195294477149b175136966d4cc4699
piet-adick <piet.adick@web.de>
2022-12-10 20:04:18 +0100

    Generated code for vlc_encode with a hand written cooperative group scan

--- cuda-examples/vlc_encode_cg.cu
+++ cuda-examples/vlc_encode_cg.cu
@@ -1,5 +1,5 @@
-// code generated from ../examples/infer/huffman/vlc_encode.desc
-// 3 __syncthreads operations have been inserted manually
+// code generated from ../examples/infer/huffman/vlc_encode.desc with a hand written shfl scan
+// 2 __syncthreads operations have been inserted manually
 
 #include "descend.cuh"
 auto vlc_encode(const descend::u32 *const h_source_data,
@@ -35,7 +35,7 @@ auto vlc_encode(const descend::u32 *const h_source_data,
 
                         __shared__ descend::u32 sm_cw[256];
                         __shared__ descend::u32 sm_cwl[256];
-                        __shared__ descend::u32 sm_res_location[256];
+                        __shared__ descend::u32 sm_scan_arr[7];
                         __shared__ descend::u32 sm_block_enc[256];
                         __shared__ descend::u32 sm_kcmax[1];
                         {
@@ -51,8 +51,6 @@ auto vlc_encode(const descend::u32 *const h_source_data,
                             }
                             __syncthreads(); // manually inserted!
                             {
-                                descend::u32 *foo = sm_res_location;
-
                                 {
                                     codeword = ((descend::u64)0);
                                     codewordlen = 0u;
@@ -78,72 +76,102 @@ auto vlc_encode(const descend::u32 *const h_source_data,
                                     tmpcwl = sm_cwl[tmp_d_item_i];
                                     codeword = ((codeword << tmpcwl) | (descend::u64)(tmpcw));
                                     codewordlen = (codewordlen + tmpcwl);
-                                    (&(*foo))[threadIdx.x] = codewordlen;
                                 }
                             }
-                            __syncthreads(); // manually inserted!
+
+
+
+                            // begin hand written cg shfl scan
+
+                            descend::u32 scan_codewordlen;
+                            descend::u32 tmp_shfl_res;
+                            auto warp = cg::tiled_partition<32>(cg::this_thread_block());
+
                             {
-                                descend::u32 *foo = sm_res_location;
-                                for (std::size_t d = 128; (d > 0u); d = (d / 2u)) {
-
-                                    if ((threadIdx.x < d)) {
-                                        (&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                   ((256 / d) - 1))] =
-                                                ((&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                            ((256 / d) - 1))] +
-                                                 (&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                            ((128 / d) - 1))]);
-                                    } else {
+                                scan_codewordlen = codewordlen;
+                                for (std::size_t k = 1; (k <= 16); k = (k * 2)) {
+                                    tmp_shfl_res = warp.shfl_up(scan_codewordlen, k);
+                                    if (warp.thread_rank() < k) {
                                     }
-                                    __syncthreads();
+                                    else {
+                                        scan_codewordlen = scan_codewordlen + tmp_shfl_res;
+                                    }
                                 }
                             }
 
-                            if ((threadIdx.x < 1)) {
-                                sm_res_location[((threadIdx.x - 0) + (256 - 1))] = 0u;
-                            } else {
+                            if (warp.meta_group_rank() < 7) {
+                                if (warp.thread_rank() < 31) {
+                                }
+                                else {
+                                    sm_scan_arr[warp.meta_group_rank()] = scan_codewordlen;
+                                }
                             }
+                            else {
+                            }
+
+
                             __syncthreads();
-                            {
-                                descend::u32 *foo = sm_res_location;
-                                for (std::size_t d = 1; (d <= 128); d = (d * 2u)) {
 
-                                    if ((threadIdx.x < d)) {
-                                        const const descend::u32 t = (&(*foo))[(
-                                                ((threadIdx.x - 0) * (256 / d)) + ((128 / d) - 1))];
-                                        (&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                   ((128 / d) - 1))] =
-                                                (&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                           ((256 / d) - 1))];
-                                        (&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                   ((256 / d) - 1))] =
-                                                ((&(*foo))[(((threadIdx.x - 0) * (256 / d)) +
-                                                            ((256 / d) - 1))] +
-                                                 t);
-                                    } else {
+                            if (warp.meta_group_rank() < 1) {
+                                descend::u32 scan_block;
+                                if (warp.thread_rank() < 7) {
+                                    scan_block = sm_scan_arr[warp.thread_rank()];
+                                }
+                                else {
+                                    scan_block = 0;
+                                }
+                                for (std::size_t k = 1; (k <= 4); k = (k * 2)) {
+                                    tmp_shfl_res = warp.shfl_up(scan_block, k);
+                                    if (warp.thread_rank() < k) {
                                     }
-                                    __syncthreads();
+                                    else {
+                                        scan_block = scan_block + tmp_shfl_res;
+                                    }
+                                }
+                                if (warp.thread_rank() < 7) {
+                                    sm_scan_arr[warp.thread_rank()] = scan_block;
+                                }
+                                else {
                                 }
                             }
+                            else {
+                            }
+
+                            __syncthreads();
+
+                            if (warp.meta_group_rank() < 1) {
+                            }
+                            else {
+                                scan_codewordlen = scan_codewordlen + sm_scan_arr[warp.meta_group_rank() - 1];
+                            }
+
+                            {
+                                scan_codewordlen = scan_codewordlen - codewordlen;
+                            }
+
+                            // end hand written cg shfl scan
+
+
+
                             if ((threadIdx.x < 255)) {
 
                             } else {
 
                                 {
                                     p4[((blockIdx.x * 1) + (threadIdx.x - 255))] =
-                                            (sm_res_location[255] + codewordlen);
+                                            (scan_codewordlen + codewordlen);
                                     sm_kcmax[(threadIdx.x - 255)] =
-                                            ((sm_res_location[255] + codewordlen) / 32u);
+                                            (scan_codewordlen + codewordlen) / 32u;
                                 }
                             }
                             __syncthreads();
-
                             {
-                                kc = (sm_res_location[threadIdx.x] / 32u);
-                                startbit = (sm_res_location[threadIdx.x] % 32u);
+                                kc = (scan_codewordlen / 32);
+                                startbit = (scan_codewordlen % 32);
                                 descend::atomic_store((&sm_block_enc[threadIdx.x]), 0u);
                             }
                             __syncthreads(); // manually inserted!
+
                             {
                                 descend::u32 wrbits;
                                 if ((codewordlen > (32u - startbit))) {
