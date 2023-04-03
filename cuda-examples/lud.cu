53b4ebc339502a39c41e3bb94456d510adcb1e3a
Lion Sostmann <lsostmann@uni-muenster.de>
2023-04-03 13:06:38 +0200

    Fehler weiterbehoben

--- cuda-examples/lud.cu
+++ cuda-examples/lud.cu
@@ -6,6 +6,9 @@ template <std::size_t tile_size, std::size_t matrix_dim>
 __host__ auto lud_descend(descend::array<descend::f32, matrix_dim> *const m5)
     -> void;
 template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
+__global__ auto lud_diagonal(descend::array<descend::f32, matrix_dim> *const m)
+    -> void;
+template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
 __device__ auto
 lud_perimeter(descend::array<descend::f32, matrix_dim> *const m2,
               descend::array<descend::array<descend::f32, tile_size>, tile_size>
@@ -28,11 +31,36 @@ __host__ auto lud_descend(descend::array<descend::f32, matrix_dim> *const m5)
       descend::array<descend::array<descend::f32, matrix_dim>, matrix_dim>>(
       (&gpu), (&(*m5)));
   static_for<0, matrix_dim / tile_size - 1>([](auto it) -> void {
+    lud_diagonal<it, tile_size, matrix_dim>
+        <<<dim3(1, 1, 1), dim3(tile_size, 1, 1),
+           0 + 4 * 1 * tile_size * tile_size * 1>>>((&m_gpu));
     lud_internal<it, tile_size, matrix_dim>((&m_gpu));
   });
+  lud_diagonal<matrix_dim / tile_size - 1, tile_size, matrix_dim>
+      <<<dim3(1, 1, 1), dim3(tile_size, 1, 1),
+         0 + 4 * 1 * tile_size * tile_size * 1>>>((&m_gpu));
   descend::copy_to_host<
       descend::array<descend::array<descend::f32, matrix_dim>, matrix_dim>>(
       (&m_gpu), m5);
+}
+
+template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
+__global__ auto lud_diagonal(descend::array<descend::f32, matrix_dim> *const m)
+    -> void {
+  extern __shared__ descend::byte $buffer[];
+  descend::f32 *const local_tile = (descend::f32 *)(&$buffer[0]);
+
+  const auto row_of_tiles = (*(*m[it * tile_size]));
+  const auto position_of_tile = (*row_of_tiles);
+
+  {
+    const auto tile = (*position_of_tile[blockIdx.x - 0]);
+    const auto local_tile_in_block = (*(*local_tile[blockIdx.x - 0]));
+    diagonal_copy_to_local_mem<tile_size>((&(*tile)),
+                                          (&(*local_tile_in_block)));
+    diagonal_block<tile_size>((&(*local_tile_in_block)));
+    diagonal_copy_to_global_mem<tile_size>(tile, local_tile_in_block);
+  }
 }
 
 template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
@@ -46,6 +74,10 @@ lud_perimeter(descend::array<descend::f32, matrix_dim> *const m2,
                   *const dia) -> void {
 
   const auto position_of_tile = (*(*(*m2[0 + it * tile_size])));
+  const auto dia_global = (*position_of_tile .0 [0]);
+  const auto peri_row_global = (*position_of_tile .1);
+
+  const auto peri_row_global_reborrow = (*peri_row_global);
 }
 
 template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
