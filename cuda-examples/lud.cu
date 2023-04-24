714cc5d3eb81232d0c45e95ac2525ad685dd97d7
Lion Sostmann <lsostmann@uni-muenster.de>
2023-04-24 14:05:18 +0200

    Code verbessert nach überprüfen der generierten Indexberechnungen

--- cuda-examples/lud.cu
+++ cuda-examples/lud.cu
@@ -9,14 +9,7 @@ __global__ auto lud_diagonal(descend::f32 *const m) -> void;
 template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
 __global__ auto lud_perimeter(descend::f32 *const m2) -> void;
 template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
-__device__ auto lud_internal(
-    descend::array<descend::f32, matrix_dim> *const m3,
-    descend::array<
-        descend::array<descend::array<descend::f32, tile_size>, tile_size>,
-        (((matrix_dim / tile_size) - it) - 1)> *const peri_row,
-    descend::array<
-        descend::array<descend::array<descend::f32, tile_size>, tile_size>,
-        (((matrix_dim / tile_size) - it) - 1)> *const peri_col) -> void;
+__global__ auto lud_internal(descend::f32 *const m3) -> void;
 /*
 function definitions
 */
@@ -38,6 +31,16 @@ __host__ auto lud_descend(descend::f32 *const m5) -> void {
              (4 * (1 * ((tile_size * tile_size) *
                         (((matrix_dim / tile_size) - it) - 1))))) +
             (4 * (1 * ((tile_size * tile_size) *
+                       (((matrix_dim / tile_size) - it) - 1)))))>>>((&m_gpu));
+    lud_internal<it, tile_size, matrix_dim>
+        <<<dim3((((matrix_dim / tile_size) - it) - 1),
+                (((matrix_dim / tile_size) - it) - 1), 1),
+           dim3(tile_size, tile_size, 1),
+           ((0 + (4 * (1 * (((tile_size * tile_size) *
+                             (((matrix_dim / tile_size) - it) - 1)) *
+                            (((matrix_dim / tile_size) - it) - 1))))) +
+            (4 * (1 * (((tile_size * tile_size) *
+                        (((matrix_dim / tile_size) - it) - 1)) *
                        (((matrix_dim / tile_size) - it) - 1)))))>>>((&m_gpu));
   });
   lud_diagonal<((matrix_dim / tile_size) - 1), tile_size, matrix_dim>
@@ -68,26 +71,25 @@ __global__ auto lud_diagonal(descend::f32 *const m) -> void {
     static_for<0, (tile_size - 1)>([&](auto i) -> void {
       {
 
-        if ((threadIdx.x - 0) < (tile_size - (i + 1))) {
+        if ((threadIdx.x - 0) < (i + 1)) {
         } else {
           {
-            const auto elem = (&(*local_tile[(
-                ((((blockIdx.x - 0) * tile_size) +
-                  ((threadIdx.x - (0 + (tile_size - (i + 1)))) + (i + 1))) *
-                 tile_size) +
-                (0 + i))]));
+            const auto elem =
+                (&(*local_tile[(((((blockIdx.x - 0) * tile_size) +
+                                  ((threadIdx.x - (0 + (i + 1))) + (i + 1))) *
+                                 tile_size) +
+                                (0 + i))]));
 
             static_for<0, i>([&](auto j) -> void {
               (*elem) =
                   (*elem) -
                   (*local_tile[(((((blockIdx.x - 0) * tile_size) +
-                                  ((threadIdx.x - (0 + (tile_size - (i + 1)))) +
-                                   (i + 1))) *
+                                  ((threadIdx.x - (0 + (i + 1))) + (i + 1))) *
                                  tile_size) +
                                 j)]) *
                       (*local_tile[(
-                          ((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
-                          j)]);
+                          ((((blockIdx.x - 0) * tile_size) + j) * tile_size) +
+                          i)]);
             });
             (*elem) =
                 (*elem) /
@@ -124,13 +126,11 @@ __global__ auto lud_diagonal(descend::f32 *const m) -> void {
     });
     {
 
-      static_for<0, tile_size>([&](auto i) -> void {
-        (*m[((((it * tile_size) + (threadIdx.x - 0)) * matrix_dim) +
-             ((((blockIdx.x - 0) + it) * tile_size) + i))]) =
-            (*local_tile[(
-                ((((blockIdx.x - 0) * tile_size) + (threadIdx.x - 0)) *
-                 tile_size) +
-                i)]);
+      static_for<1, tile_size>([&](auto i) -> void {
+        (*m[((((it * tile_size) + i) * matrix_dim) +
+             ((((blockIdx.x - 0) + it) * tile_size) + (threadIdx.x - 0)))]) =
+            (*local_tile[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                          (threadIdx.x - 0))]);
       });
     }
   }
@@ -144,33 +144,161 @@ __global__ auto lud_perimeter(descend::f32 *const m2) -> void {
       1 * ((tile_size * tile_size) * (((matrix_dim / tile_size) - it) - 1)))]);
   descend::f32 *const dia = (descend::f32 *)(&peri_col[(
       1 * ((tile_size * tile_size) * (((matrix_dim / tile_size) - it) - 1)))]);
+
+  {
+
+    if ((threadIdx.x - 0) < tile_size) {
+      {
+
+        static_for<0, (tile_size / 2)>([&](auto i) -> void {
+          (*dia[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                 (threadIdx.x - 0))]) =
+              (*m2[(((((0 + it) * tile_size) + i) * matrix_dim) +
+                    (((0 + it) * tile_size) + (threadIdx.x - 0)))]);
+        });
+        static_for<0, tile_size>([&](auto i) -> void {
+          (*peri_row[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                      (threadIdx.x - 0))]) =
+              (*m2[(((((0 + it) * tile_size) + i) * matrix_dim) +
+                    (((((blockIdx.x - 0) + 1) + it) * tile_size) +
+                     (threadIdx.x - 0)))]);
+        });
+      }
+    } else {
+      {
+
+        static_for<0, (tile_size - (tile_size / 2))>([&](auto i) -> void {
+          (*dia[(((((blockIdx.x - 0) * tile_size) + (i + (tile_size / 2))) *
+                  tile_size) +
+                 (threadIdx.x - (0 + tile_size)))]) =
+              (*m2[(
+                  ((((0 + it) * tile_size) + (i + (tile_size / 2))) *
+                   matrix_dim) +
+                  (((0 + it) * tile_size) + (threadIdx.x - (0 + tile_size))))]);
+        });
+        static_for<0, tile_size>([&](auto i) -> void {
+          (*peri_col[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                      (threadIdx.x - (0 + tile_size)))]) =
+              (*m2[(
+                  ((((((blockIdx.x - 0) + 1) + it) * tile_size) + i) *
+                   matrix_dim) +
+                  (((0 + it) * tile_size) + (threadIdx.x - (0 + tile_size))))]);
+        });
+      }
+    }
+
+    __syncthreads();
+
+    if ((threadIdx.x - 0) < tile_size) {
+      {
+        static_for<1, tile_size>([&](auto i) -> void {
+          static_for<0, i>([&](auto j) -> void {
+            (*peri_row[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                        (threadIdx.x - 0))]) =
+                (*peri_row[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                            (threadIdx.x - 0))]) -
+                (*dia[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                       j)]) *
+                    (*peri_row[(
+                        ((((blockIdx.x - 0) * tile_size) + j) * tile_size) +
+                        (threadIdx.x - 0))]);
+          });
+        });
+      }
+    } else {
+      {
+        static_for<0, tile_size>([&](auto i) -> void {
+          static_for<0, i>([&](auto j) -> void {
+            (*peri_col[(((((blockIdx.x - 0) * tile_size) +
+                          (threadIdx.x - (0 + tile_size))) *
+                         tile_size) +
+                        i)]) =
+                (*peri_col[(((((blockIdx.x - 0) * tile_size) +
+                              (threadIdx.x - (0 + tile_size))) *
+                             tile_size) +
+                            i)]) -
+                (*dia[(((((blockIdx.x - 0) * tile_size) + j) * tile_size) +
+                       i)]) *
+                    (*peri_col[(((((blockIdx.x - 0) * tile_size) +
+                                  (threadIdx.x - (0 + tile_size))) *
+                                 tile_size) +
+                                j)]);
+          });
+          (*peri_col[(((((blockIdx.x - 0) * tile_size) +
+                        (threadIdx.x - (0 + tile_size))) *
+                       tile_size) +
+                      i)]) =
+              (*peri_col[(((((blockIdx.x - 0) * tile_size) +
+                            (threadIdx.x - (0 + tile_size))) *
+                           tile_size) +
+                          i)]) /
+              (*dia[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) + i)]);
+        });
+      }
+    }
+
+    __syncthreads();
+    if ((threadIdx.x - 0) < tile_size) {
+      {
+
+        static_for<1, tile_size>([&](auto i) -> void {
+          (*m2[(((((0 + it) * tile_size) + i) * matrix_dim) +
+                (((((blockIdx.x - 0) + 1) + it) * tile_size) +
+                 (threadIdx.x - 0)))]) =
+              (*peri_row[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                          (threadIdx.x - 0))]);
+        });
+      }
+    } else {
+      {
+
+        static_for<0, tile_size>([&](auto i) -> void {
+          (*m2[(
+              ((((((blockIdx.x - 0) + 1) + it) * tile_size) + i) * matrix_dim) +
+              (((0 + it) * tile_size) + (threadIdx.x - (0 + tile_size))))]) =
+              (*peri_col[(((((blockIdx.x - 0) * tile_size) + i) * tile_size) +
+                          (threadIdx.x - (0 + tile_size)))]);
+        });
+      }
+    }
+  }
 }
 
 template <std::size_t it, std::size_t tile_size, std::size_t matrix_dim>
-__device__ auto lud_internal(
-    descend::array<descend::f32, matrix_dim> *const m3,
-    descend::array<
-        descend::array<descend::array<descend::f32, tile_size>, tile_size>,
-        (((matrix_dim / tile_size) - it) - 1)> *const peri_row,
-    descend::array<
-        descend::array<descend::array<descend::f32, tile_size>, tile_size>,
-        (((matrix_dim / tile_size) - it) - 1)> *const peri_col) -> void {
+__global__ auto lud_internal(descend::f32 *const m3) -> void {
+  extern __shared__ descend::byte $buffer[];
+  descend::f32 *const peri_row = (descend::f32 *)(&$buffer[0]);
+  descend::f32 *const peri_col = (descend::f32 *)(&peri_row[(
+      1 * (((tile_size * tile_size) * (((matrix_dim / tile_size) - it) - 1)) *
+           (((matrix_dim / tile_size) - it) - 1)))]);
 
-  {{{{const auto peri_row_shared_block = (&(*peri_row[(blockIdx.y - 0)][(
-          blockIdx.x - 0)][(threadIdx.y - 0)][(threadIdx.x - 0)]));
-  const auto peri_col_shared_block =
-      (&(*peri_col[(blockIdx.y - 0)][(blockIdx.x - 0)][(threadIdx.y - 0)]
-                  [(threadIdx.x - 0)]));
-  const auto peri_row_global_block =
-      (&(*m3[(((0 + it) * tile_size) + (threadIdx.y - 0))][(
-          ((((blockIdx.x - 0) + 1) + it) * tile_size) + (threadIdx.x - 0))]));
-  const auto peri_col_global_block =
-      (&(*m3[(((((blockIdx.y - 0) + 1) + it) * tile_size) + (threadIdx.y - 0))]
-            [(((0 + it) * tile_size) + (threadIdx.x - 0))]));
+  {{{{const auto peri_row_shared_block = (&(*peri_row[(
+          ((((((blockIdx.y - 0) * (((matrix_dim / tile_size) - it) - 1)) +
+              (blockIdx.x - 0)) *
+             tile_size) +
+            (threadIdx.y - 0)) *
+           tile_size) +
+          (threadIdx.x - 0))]));
+  const auto peri_col_shared_block = (&(*peri_col[(
+      ((((((blockIdx.y - 0) * (((matrix_dim / tile_size) - it) - 1)) +
+          (blockIdx.x - 0)) *
+         tile_size) +
+        (threadIdx.y - 0)) *
+       tile_size) +
+      (threadIdx.x - 0))]));
+  const auto peri_row_global_block = (&(*m3[(
+      ((((0 + it) * tile_size) + (threadIdx.y - 0)) * matrix_dim) +
+      (((((blockIdx.x - 0) + 1) + it) * tile_size) + (threadIdx.x - 0)))]));
+  const auto peri_col_global_block = (&(
+      *m3[(((((((blockIdx.y - 0) + 1) + it) * tile_size) + (threadIdx.y - 0)) *
+            matrix_dim) +
+           (((0 + it) * tile_size) + (threadIdx.x - 0)))]));
   (*peri_row_shared_block) = (*peri_row_global_block);
   (*peri_col_shared_block) = (*peri_col_global_block);
 }
 }
+
+__syncthreads();
 }
 }
 
@@ -179,17 +307,41 @@ __device__ auto lud_internal(
     {
       {
         const auto thread_element = (&(*m3[(
-            ((((blockIdx.y - 0) + 1) + it) * tile_size) + (threadIdx.y - 0))][(
-            ((((blockIdx.x - 0) + 1) + it) * tile_size) + (threadIdx.x - 0))]));
-        auto sum = (*peri_col[(blockIdx.y - 0)][(blockIdx.x - 0)]
-                             [(threadIdx.y - 0)][0]) *
-                   (*peri_row[(blockIdx.y - 0)][(blockIdx.x - 0)]
-                             [(threadIdx.x - 0)][0]);
+            ((((((blockIdx.y - 0) + 1) + it) * tile_size) + (threadIdx.y - 0)) *
+             matrix_dim) +
+            (((((blockIdx.x - 0) + 1) + it) * tile_size) +
+             (threadIdx.x - 0)))]));
+        auto sum =
+            (*peri_col[(
+                ((((((blockIdx.y - 0) * (((matrix_dim / tile_size) - it) - 1)) +
+                    (blockIdx.x - 0)) *
+                   tile_size) +
+                  (threadIdx.y - 0)) *
+                 tile_size) +
+                0)]) *
+            (*peri_row[(
+                ((((((blockIdx.y - 0) * (((matrix_dim / tile_size) - it) - 1)) +
+                    (blockIdx.x - 0)) *
+                   tile_size) +
+                  0) *
+                 tile_size) +
+                (threadIdx.x - 0))]);
         static_for<1, tile_size>([&](auto i) -> void {
-          sum = sum + (*peri_col[(blockIdx.y - 0)][(blockIdx.x - 0)]
-                                [(threadIdx.y - 0)][i]) *
-                          (*peri_row[(blockIdx.y - 0)][(blockIdx.x - 0)]
-                                    [(threadIdx.x - 0)][i]);
+          sum = sum +
+                (*peri_col[(((((((blockIdx.y - 0) *
+                                 (((matrix_dim / tile_size) - it) - 1)) +
+                                (blockIdx.x - 0)) *
+                               tile_size) +
+                              (threadIdx.y - 0)) *
+                             tile_size) +
+                            i)]) *
+                    (*peri_row[(((((((blockIdx.y - 0) *
+                                     (((matrix_dim / tile_size) - it) - 1)) +
+                                    (blockIdx.x - 0)) *
+                                   tile_size) +
+                                  i) *
+                                 tile_size) +
+                                (threadIdx.x - 0))]);
         });
         (*thread_element) = (*thread_element) - sum;
       }
